4c92f7ded487d6c649abfb2eb565a40d88c06287
Reemhusein <88857357+Reemhusein@users.noreply.github.com>
2021-08-20 12:33:11 +0300

    Update Employee.java

--- src/main/java/com/ensat/entities/Employee.java
+++ src/main/java/com/ensat/entities/Employee.java
@@ -3,6 +3,24 @@ package com.ensat.entities;
 import javax.persistence.*;
 import java.util.Collection;
 
+import com.zaxxer.hikari.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+  @Value("${spring.datasource.url}")
+  private String dbUrl;
+
+  @Bean
+  public DataSource dataSource() {
+      HikariConfig config = new HikariConfig();
+      config.setJdbcUrl(dbUrl);
+      return new HikariDataSource(config);
+  }
+}
 /**
  * Employee entity.
  */
